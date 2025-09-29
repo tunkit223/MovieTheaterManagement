b345acbd071976af54407330771eb74f7247850d
Kiệt Trương Tuấn <tuankietwork22@gmail.com>
2025-09-29 10:18:25 +0700

    Push-Frontend-Backend-Default

---
+++ MovieTheaterManagemant-Backend/src/main/java/com/javaProject/MovieTheaterManagemant/MovieTheaterManagemantApplication.java
@@ -0,0 +1,13 @@
+package com.javaProject.MovieTheaterManagemant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTheaterManagemantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTheaterManagemantApplication.class, args);
+	}
+
+}
---
+++ MovieTheaterManagemant-Backend/src/test/java/com/javaProject/MovieTheaterManagemant/MovieTheaterManagemantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaProject.MovieTheaterManagemant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterManagemantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
